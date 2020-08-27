4728cd459d3267188e7f29cdbd8962329e5589ce
jasshans <boggdanmail@gmail.com>
2020-08-28 00:18:05 +0100

    Update sacctomari.js

--- sacctomari.js
+++ sacctomari.js
@@ -1,4 +1,4 @@
-function createSaleIdDiv(saleId) {
+function createAccountIdDiv(accountId) {
 
     //Create link
 	var protocol = window.location.protocol;
@@ -6,55 +6,55 @@ function createSaleIdDiv(saleId) {
 	var a = document.createElement('a');
 
     if (url.indexOf('localhost') >= 0) {
-        a.href = 'http://' + url + ':9090//mari/#/hotel/' + saleId;
+        a.href = 'http://' + url + ':9090//mari/#/hotel/' + accountId;
     } else if (url.indexOf('staging') >= 0 || url.indexOf('uat') >= 0) {
-        a.href = protocol + '//' + url + '/mari/#/hotel/' + saleId;
+        a.href = protocol + '//' + url + '/mari/#/hotel/' + accountId;
     } else {
-        a.href = 'https://mari.secretescapes.com/#/hotel/' + saleId;
+        a.href = 'https://mari.secretescapes.com/#/hotel/' + accountId;
     }
 
     //Info displayed
 	a.appendChild(document.createTextNode('- ID -'));
 	a.appendChild(document.createElement('br'));
-	a.appendChild(document.createTextNode(saleId));
+	a.appendChild(document.createTextNode(accountId));
 
     //Styling
-	var saleIdDiv = document.createElement('div');
-	saleIdDiv.id = 'se-sale-id-revealer';
-	saleIdDiv.style.backgroundColor = "#FFFFFF";
-	saleIdDiv.style.fontFamily = "'Open Sans', Sans-serif";
-	saleIdDiv.style.fontSize = "18px";
-	saleIdDiv.style.float = "left";
-	saleIdDiv.style.textAlign = "center";
-	saleIdDiv.style.position = "fixed";
-	saleIdDiv.style.left = "30px";
-	saleIdDiv.style.top = "30px";
-	saleIdDiv.style.padding = "15px";
-	saleIdDiv.style.borderRadius = "4px";
-	saleIdDiv.style.zIndex = "1000";
-	saleIdDiv.style.border = "1px solid rgb(176, 176, 176)";
-	saleIdDiv.appendChild(a);
-	document.body.insertBefore(saleIdDiv, document.body.childNodes[0]);
+	var accountIdDiv = document.createElement('div');
+	accountIdDiv.id = 'se-sale-id-revealer';
+	accountIdDiv.style.backgroundColor = "#FFFFFF";
+	accountIdDiv.style.fontFamily = "'Open Sans', Sans-serif";
+	accountIdDiv.style.fontSize = "18px";
+	accountIdDiv.style.float = "left";
+	accountIdDiv.style.textAlign = "center";
+	accountIdDiv.style.position = "fixed";
+	accountIdDiv.style.left = "30px";
+	accountIdDiv.style.top = "30px";
+	accountIdDiv.style.padding = "15px";
+	accountIdDiv.style.borderRadius = "4px";
+	accountIdDiv.style.zIndex = "1000";
+	accountIdDiv.style.border = "1px solid rgb(176, 176, 176)";
+	accountIdDiv.appendChild(a);
+	document.body.insertBefore(accountIdDiv, document.body.childNodes[0]);
 }
 
 // This is needed for Chrome, at least, because it does not give direct access
 // to a website's JavaScript variables.
 function updateDivByScriptInjection() {
-	var getSaleIdFunction = '(function() {' + createSaleIdDiv + ' createSaleIdDiv(window.SE.saleId); })();';
+	var getAccountIdFunction = '(function() {' + createAccountIdDiv + ' createAccountIdDiv(window.SE.accountId); })();';
 
 	var script = document.createElement('script');
-	script.textContent = getSaleIdFunction;
+	script.textContent = getAccountIdFunction;
 	(document.head||document.documentElement).appendChild(script);
 	script.parentNode.removeChild(script);
 }
 
-function addSaleIdToPage() {
+function addAccountIdToPage() {
 	if(typeof SE != 'undefined') {
-		createSaleIdDiv(SE.saleId);
+		createAccountIdDiv(SE.accountId);
 	}
 	else {
 		updateDivByScriptInjection();
 	}
 }
 
-addSaleIdToPage();
+addAccountIdToPage();
